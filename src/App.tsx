34445cc2c246cfca07e7a77d2e6f26cc8a0c4257
dushyant kumar <dushyantvelar@gmail.com>
2026-04-09 11:41:35 +0530

    setup the shadcn successfuly

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="bg-black w-full h-screen">
+      App
+      <Button
+        variant="outline"
+        className="border-green-500 rounded-md border-2 hover:bg-green-600 bg-gray-700 text-white"
+      >
+        Click
+      </Button>
+    </div>
+  );
+};
 
-export default App
+export default App;
